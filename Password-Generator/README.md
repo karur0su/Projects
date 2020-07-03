6cf68850a253d60f40980205f552904578e118d0
David White Wolf <mkarurosun@gmail.com>
2020-07-02 23:03:07 -0400

    Final Update

--- Password-Generator/README.md
+++ Password-Generator/README.md
@@ -1,16 +1,17 @@
 Password Generator
 # 03 JavaScript: Password Generator
 
-Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+An application that generates a random password based on user-selected criteria. 
 
-If you are unfamiliar with special characters, take a look at [some examples](https://www.owasp.org/index.php/Password_special_characters).
 
 ## User Story
 
 ```
 AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
+
+"I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security"
+
 ```
 
 ## Acceptance Criteria
@@ -30,26 +31,14 @@ WHEN prompted for character types to include in the password
 
 THEN I choose lowercase, uppercase, numeric, and/or special characters
 
-
 WHEN I answer each prompt
+
 THEN my input should be validated and at least one character type should be selected
+
 WHEN all prompts are answered
+
 THEN a password is generated that matches the selected criteria
+
 WHEN the password is generated
+
 THEN the password is either displayed in an alert or written to the page
-```
-
-The following image demonstrates the application functionality:
-
-![password generator demo](./Assets/03-javascript-homework-demo.png)
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -1,42 +1,58 @@
-// The Password generator will let user choose a length of at least 8 characters and no more than 128 characters.
+// The Password generator will let the user choose boundary conditions.  
 
-//Assignment Code + Event Listener to prompt questions when button pushed
+
+// Assignment Code + Event Listener to prompt questions when button pushed.
 document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Various Arrays 
+
+
+// Arrays. 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Variable Declaration 
+
+
+// Variable Declaration.  
 var confirmLength = "";
 var confirmSpecialCharacter;
 var confirmNumericCharacter;
 var confirmUpperCase;
 var confirmLowerCase;
 
-// The length of at least 8 characters and no more than 128 characters
+
+
+
+
+
+// The length of at least 8 characters and no more than 128 characters. 
 
 function generatePassword() {
   var confirmLength = (prompt("How many characters would you like your password to contain? (8-128 characters)"));
 
-  // Loop if user answers are outside the established boundary conditions 
+  // Loop if user answers are outside the established boundary conditions. 
   while(confirmLength <= 7 || confirmLength >= 129) {
       alert("Password length must be between 8-128 characters please try again.");
       var confirmLength = (prompt("How many characters would you like your password to contain? (8-128)"));
       } 
 
-      // Repeat back how many charactes the user will have  
+    
+      // Repeat back how many characters the user will have.  
       alert(`Your password will have ${confirmLength} characters`);
 
-    // Determine parameters of password
+    
+    
+    
+      // Determine parameters of password.
     var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
     var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters"); 
     var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
     var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
 
-      // Loop if answer is outside the parameters 
+    
+    
+    // Loop if answer is outside the parameters. 
       while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
         alert("You must choose at least one parameter");
         var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
@@ -45,7 +61,9 @@ function generatePassword() {
         var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
     } 
 
-      // Assign an action to the password parameters NEED TO FIX THIS
+     
+    
+    // Assign an action to the password parameters "NEED TO FIX THIS".
       var passwordCharacters = []
       
     if (confirmSpecialCharacter) {
@@ -66,7 +84,9 @@ function generatePassword() {
 
       console.log(passwordCharacters)
 
-      // Empty string to be filled based on for loop selecting random characters from the array
+     
+     
+      // Empty string to be filled based on for loop selecting random characters from the array.
       var randomPassword = ""
       
       for (var i = 0; i < confirmLength; i++) {
@@ -76,7 +96,7 @@ function generatePassword() {
       return randomPassword;
 }
 
-// Write password to the #password input
+// Write password to the #password input.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
