6bd5690210f99c478605ebead127d9990ba1d76c
David White Wolf <mkarurosun@gmail.com>
2020-07-02 20:59:07 -0400

    File Update

--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -25,18 +25,6 @@ function generate(){
 //set default length display of 25
 document.getElementById("length").innerHTML = "Length: 25";
 
-//function to set length based on slider position
-document.getElementById("slider").oninput = function(){
-
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
-    }
-    else{
-        document.getElementById("length").innerHTML = "Length: 1";
-    }
-
-}
-
 //function to copy password to clipboard
 function copyPassword(){
 
